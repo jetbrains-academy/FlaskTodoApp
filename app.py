d94f2e40d5673cba58aa0e593f45228dd1a98330
Alice <alice@example.com>
2025-03-20 21:15:24 +0200

    reorganized code a bit

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
-from flask_sqlalchemy import SQLAlchemy
+from database_initialization import db  # Import db from the new extensions module
+from models import Project, Task
 
 # Constants for configuration
 DATABASE_URI = 'sqlite:///todo.db'
@@ -9,27 +10,7 @@ TRACK_MODIFICATIONS = False
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URI
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = TRACK_MODIFICATIONS
-db = SQLAlchemy(app)
-
-
-# Models
-class Project(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(200), nullable=False)
-    todos = db.relationship('Todo', backref='project', lazy=True)
-
-    def __repr__(self):
-        return f'<Project {self.id}: {self.name}>'
-
-
-class Todo(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    task = db.Column(db.String(200), nullable=False)
-    complete = db.Column(db.Boolean, default=False)
-    project_id = db.Column(db.Integer, db.ForeignKey('project.id'), nullable=False)
-
-    def __repr__(self):
-        return f'<Task {self.id}: {self.task} in Project {self.project_id}>'
+db.init_app(app)  # Bind the database to the app
 
 
 # Extracted function: Initializes the database
@@ -53,7 +34,7 @@ def home():
 def add_task():
     task_content = request.form.get('task')
     if task_content:
-        new_task = Todo(task=task_content, complete=False)
+        new_task = Task(task=task_content, complete=False)
         db.session.add(new_task)
         db.session.commit()
     return redirect(url_for('home'))
@@ -71,7 +52,7 @@ def create_project():
 
 @app.route('/toggle_task_complete/<int:task_id>')
 def toggle_task_complete(task_id):
-    task = Todo.query.get_or_404(task_id)
+    task = Task.query.get_or_404(task_id)
     task.complete = not task.complete
     db.session.commit()
     return redirect(url_for('project_tasks', project_id=task.project_id))
@@ -79,7 +60,7 @@ def toggle_task_complete(task_id):
 
 @app.route('/delete_task/<int:task_id>')
 def delete_task(task_id):
-    task = Todo.query.get_or_404(task_id)
+    task = Task.query.get_or_404(task_id)
     db.session.delete(task)
     db.session.commit()
     return redirect(url_for('project_tasks', project_id=task.project_id))
@@ -90,7 +71,7 @@ def add_project_task(project_id):
     project = Project.query.get_or_404(project_id)
     task_content = request.form.get('task')
     if task_content:
-        new_task = Todo(task=task_content, complete=False, project_id=project_id)
+        new_task = Task(task=task_content, complete=False, project_id=project_id)
         db.session.add(new_task)
         db.session.commit()
     return redirect(url_for('project_tasks', project_id=project_id))
@@ -99,7 +80,7 @@ def add_project_task(project_id):
 @app.route('/delete_project/<int:project_id>', methods=['POST'])
 def delete_project(project_id):
     project = Project.query.get_or_404(project_id)
-    Todo.query.filter_by(project_id=project_id).delete()  # Cascade delete tasks
+    Task.query.filter_by(project_id=project_id).delete()  # Cascade delete tasks
     db.session.delete(project)
     db.session.commit()
     return redirect(url_for('home'))
@@ -108,7 +89,7 @@ def delete_project(project_id):
 @app.route('/project/<int:project_id>')
 def project_tasks(project_id):
     project = Project.query.get_or_404(project_id)
-    tasks = Todo.query.filter_by(project_id=project_id).all()
+    tasks = Task.query.filter_by(project_id=project_id).all()
     return render_template('project_tasks.html', project=project, tasks=tasks)
 
 
---
+++ models.py
@@ -0,0 +1,20 @@
+from database_initialization import db  # Import db from the new extensions module
+
+
+class Project(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    todos = db.relationship('Task', backref='project', lazy=True)
+
+    def __repr__(self):
+        return f'<Project {self.id}: {self.name}>'
+
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    task = db.Column(db.String(200), nullable=False)
+    complete = db.Column(db.Boolean, default=False)
+    project_id = db.Column(db.Integer, db.ForeignKey('project.id'), nullable=False)
+
+    def __repr__(self):
+        return f'<Task {self.id}: {self.task} in Project {self.project_id}>'
