da9fdee74743ebfae5de4ac2209520710895aaf2
Billy Bob <billy@example.com>
2025-03-21 11:51:57 +0200

    use camel case for variable names and functions in the project

--- models.py
+++ models.py
@@ -14,7 +14,7 @@ class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     task = db.Column(db.String(200), nullable=False)
     complete = db.Column(db.Boolean, default=False)
-    project_id = db.Column(db.Integer, db.ForeignKey('project.id'), nullable=False)
+    projectId = db.Column(db.Integer, db.ForeignKey('project.id'), nullable=False)
 
     def __repr__(self):
-        return f'<Task {self.id}: {self.task} in Project {self.project_id}>'
+        return f'<Task {self.id}: {self.task} in Project {self.projectId}>'
--- project_routes.py
+++ project_routes.py
@@ -6,25 +6,25 @@ from routes import routes_bp
 
 
 @routes_bp.route('/create_project', methods=['POST'])
-def create_project():
-    project_name = request.form.get('project_name')
-    if project_name:
-        new_project = Project(name=project_name)
-        db.session.add(new_project)
+def createProject():
+    projectName = request.form.get('project_name')
+    if projectName:
+        newProject = Project(name=projectName)
+        db.session.add(newProject)
         db.session.commit()
     return redirect(url_for('routes.home'))
 
 
 @routes_bp.route('/delete_project/<int:project_id>', methods=['POST'])
-def delete_project(project_id):
-    project = Project.query.get_or_404(project_id)
-    Task.query.filter_by(project_id=project_id).delete()  # Cascade delete tasks
+def deleteProject(projectId):
+    project = Project.query.get_or_404(projectId)
+    Task.query.filter_by(project_id=projectId).delete()  # Cascade delete tasks
     db.session.delete(project)
     db.session.commit()
     return redirect(url_for('routes.home'))
 
 @routes_bp.route('/project/<int:project_id>')
-def project_tasks(project_id):
+def projectTasks(project_id):
     project = Project.query.get_or_404(project_id)
     tasks = Task.query.filter_by(project_id=project_id).all()
     return render_template('project_tasks.html', project=project, tasks=tasks)
