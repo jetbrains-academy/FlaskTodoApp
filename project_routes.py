530d76bc9a1581ce744e530cd70e733be5738af7
Billy Bob <billy@example.com>
2025-03-27 19:47:13 +0200

    remove log functions

--- project_routes.py
+++ project_routes.py
@@ -20,15 +20,6 @@ TEMPLATE_PROJECT_DESCRIPTION = "project_description.html"
 TEMPLATE_PROJECT_TASKS = "project_tasks.html"
 
 
-# Reusable logging functions
-def log_error(message, exception):
-    logger.error("%s: %s", message, exception, exc_info=True)
-
-
-def log_info(message, *args):
-    logger.info(message, *args)
-
-
 @routes_bp.route('/create_project', methods=['POST'])
 def create_project():
     project_name = request.form.get('project_name')
@@ -37,13 +28,13 @@ def create_project():
         return redirect(url_for('routes.home'))
 
     try:
-        log_info("Creating new project with name: %s", project_name)
+        logger.info("Creating new project with name: %s", project_name)
         new_project = Project(name=project_name)
         db.session.add(new_project)
         db.session.commit()
-        log_info("Project '%s' successfully created.", project_name)
+        logger.info("Project '%s' successfully created.", project_name)
     except Exception as e:
-        log_error(ERROR_CREATING_PROJECT, e)
+        logger.error("%s: %s", ERROR_CREATING_PROJECT, e, exc_info=True)
         return ERROR_CREATING_PROJECT, 500
 
     return redirect(url_for('routes.home'))
@@ -58,14 +49,14 @@ def project_description(project_id):
 @routes_bp.route('/delete_project/<int:project_id>', methods=['POST'])
 def delete_project(project_id):
     try:
-        log_info("Attempting to delete project with ID: %d", project_id)
+        logger.info("Attempting to delete project with ID: %d", project_id)
         project_record = Project.query.get_or_404(project_id)
         Task.query.filter_by(project_id=project_id).delete()  # Cascade delete tasks
         db.session.delete(project_record)
         db.session.commit()
-        log_info("Project with ID %d successfully deleted.", project_id)
+        logger.info("Project with ID %d successfully deleted.", project_id)
     except Exception as e:
-        log_error(ERROR_DELETING_PROJECT, e)
+        logger.error("%s: %s", ERROR_DELETING_PROJECT, e, exc_info=True)
         return ERROR_DELETING_PROJECT, 500
 
     return redirect(url_for('routes.home'))
@@ -74,12 +65,12 @@ def delete_project(project_id):
 @routes_bp.route('/project/<int:project_id>')
 def project_tasks(project_id):
     try:
-        log_info("Fetching tasks for project with ID: %d", project_id)
+        logger.info("Fetching tasks for project with ID: %d", project_id)
         project_record = Project.query.get_or_404(project_id)
         tasks = Task.query.filter_by(project_id=project_id).all()
-        log_info("Found %d tasks for project ID: %d", len(tasks), project_id)
+        logger.info("Found %d tasks for project ID: %d", len(tasks), project_id)
     except Exception as e:
-        log_error(ERROR_FETCHING_TASKS, e)
+        logger.error("%s: %s", ERROR_FETCHING_TASKS, e, exc_info=True)
         return ERROR_FETCHING_TASKS, 500
 
     return render_template(TEMPLATE_PROJECT_TASKS, project=project_record, tasks=tasks)
